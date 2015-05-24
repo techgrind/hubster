6b3356696ee96e25538f780ee1958cffff248d75
Jeff Ancel <jancel@gmail.com>
2015-05-24 13:54:11 -0500

    re-arranging a tad

---
+++ frontend/app/app-module.js
@@ -0,0 +1,15 @@
+(function () {
+  'use strict';
+
+  /* @ngdoc object
+   * @name hubster
+   * @description
+   *
+   */
+  angular
+    .module('hubster', [
+      'ngAria',
+      'ui.router',
+      'home'
+    ]);
+}());
