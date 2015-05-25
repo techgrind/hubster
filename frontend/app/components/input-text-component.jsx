04d09cda8ca4481214ba7786ceb4d50d2601fea8
achernyak <artemchernyak@gmail.com>
2015-05-25 18:30:37 -0500

    input component

--- frontend/app/components/input-text-component.jsx
+++ frontend/app/components/input-text-component.jsx
@@ -2,20 +2,22 @@ var InputTextComponent = React.createClass({
   propTypes: {
     col: React.PropTypes.number,
     inputLabel: React.PropTypes.string.isRequired,
-    ngModel: React.PropTypes.string.isRequired
+    model: React.PropTypes.string.isRequired
   },
   getDefaultProps: function() {
     return {
-      col: 6,
+      col: 12,
       inputLabel: "What ever"
     };
   },
   render: function () {
     var divClass = "input-field col s" + this.props.col;
+    var inputClass = this.props.inputLabel.split(" ").join("_");
+
     return (
       <div className={divClass}>
-        <input ng-model={this.props.ngModel} type="text"/>
-        <label>{this.props.inputLabel}</label>
+        <input id={inputClass} ng-model={this.props.model} type="text"/>
+        <label for={inputClass}>{this.props.inputLabel}</label>
       </div>
     );
   }
