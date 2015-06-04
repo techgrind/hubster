2718aa62bab882df435e5bcf636a3f7e62e04b43
Jeff Ancel <jancel@gmail.com>
2015-06-04 16:54:32 -0500

    newly generated ng-poly

---
+++ hubster-new/app/app-module.js
@@ -0,0 +1,16 @@
+(function () {
+  'use strict';
+
+  /* @ngdoc object
+   * @name hubster
+   * @description
+   *
+   */
+  angular
+    .module('hubster', [
+      'ngAria',
+      'ngMaterial',
+      'ui.router',
+      'home'
+    ]);
+}());
---
+++ hubster-new/app/app-routes.js
@@ -0,0 +1,11 @@
+(function () {
+  'use strict';
+
+  angular
+    .module('hubster')
+    .config(config);
+
+  function config($urlRouterProvider) {
+    $urlRouterProvider.otherwise('/home');
+  }
+}());
