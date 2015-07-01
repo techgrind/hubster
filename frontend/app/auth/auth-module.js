fbe97a13a3fe949b50768dfa83192df2f0baa14f
Jeff Ancel <jancel@gmail.com>
2015-07-01 09:00:01 -0400

    adding oauth button functionality

--- frontend/app/auth/auth-module.js
+++ frontend/app/auth/auth-module.js
@@ -17,7 +17,13 @@
 
   function AuthProviderConfig($authProvider) {
     $authProvider.configure({
-      apiUrl: 'http://hubster-rails.dev'
+      apiUrl: 'http://hubster-rails.dev',
+      authProviderPaths: {
+        github:   '/auth/github',
+        facebook: '/auth/facebook',
+        google:   '/auth/google_oauth2',
+        twitter:  '/auth/twitter'
+      }
     });
   }
   angular.module('auth').config(AuthProviderConfig);
